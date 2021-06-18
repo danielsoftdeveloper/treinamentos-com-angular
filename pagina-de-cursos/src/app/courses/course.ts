88111da6c200e13ae43862035b822babfbe67f1c
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2021-06-18 20:05:51 -0300

    Creating courses component and table with initial list.

---
+++ pagina-de-cursos/src/app/courses/course.ts
@@ -0,0 +1,12 @@
+export class Course {
+
+    id: number | undefined;
+    name: string | undefined;
+    releaseDate: string | undefined;
+    description: string  | undefined;
+    duration: number | undefined;
+    code: string | undefined;
+    rating: number | undefined;
+    price: number | undefined;
+    imageUrl: string | undefined;
+ }
