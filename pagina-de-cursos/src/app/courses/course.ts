cb31da8fdeefd1c7aa23f3d11f8bdf2839d225ad
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2021-06-18 21:26:07 -0300

    Created star component and installed font-awesome.

--- pagina-de-cursos/src/app/courses/course.ts
+++ pagina-de-cursos/src/app/courses/course.ts
@@ -3,10 +3,10 @@ export class Course {
     id: number | undefined;
     name: string | undefined;
     releaseDate: string | undefined;
-    description: string  | undefined;
+    description: string = ' ';
     duration: number | undefined;
     code: string | undefined;
-    rating: number | undefined;
+    rating: number = 0;
     price: number | undefined;
     imageUrl: string | undefined;
  }
